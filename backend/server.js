14f94a32dc606d5cf87630b1f23631a3b67384df
drinor101 <drinorshabiu1@gmail.com>
2025-09-01 05:33:10 +0200

    fix CORS: add correct Netlify URL for startechmanagement.netlify.app

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(cors({
     'http://localhost:5173', 
     'http://localhost:5174', 
     'http://localhost:5175',
+    'https://startechmanagement.netlify.app',
     'https://startechapp.netlify.app',
     'https://startechapp.vercel.app'
   ],
