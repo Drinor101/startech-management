6c18dcb3bee8ae6fb05423507a9e29561e32f39c
drinor101 <drinorshabiu1@gmail.com>
2025-11-04 04:23:35 +0100

    fixed-cors

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.use(cors({
     'https://startech-management.onrender.com'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-User-ID'],
   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar']
 }));
